5f398faf2467cf4977d6ce48eec9fac7c880ce1f
Jomar Antonio Cardoso <jomar.cardoso@cwi.com.br>
2017-09-12 10:34:28 -0300

    feat(template): requisição via ajax

--- novos-classicos/js/novos-classicos.js
+++ novos-classicos/js/novos-classicos.js
@@ -1,31 +1,45 @@
 $(function(){
 
-  window.App = {
-    Models: {},
-    Collections: {},
-    Views: {}
-  };
+  $.ajax("../templates/novos-classicos-template-header.html").done(function(header){
+    $("body").append(header);
+    console.log($("#novos-classicos-template-header").html());
 
-  App.Models.Header = Backbone.Model.extend({
-    defaults: {}
+
+
+      window.App = {
+        Models: {},
+        Collections: {},
+        Views: {}
+      };
+
+      App.Models.Header = Backbone.Model.extend({
+        defaults: {}
+      });
+
+      App.Views.Header = Backbone.View.extend({
+        tagName: 'header',
+        template: _.template( $('#novos-classicos-template-header').html()),
+        initialize: function(){
+          // console.log(this.template(this.model.toJSON()));
+          this.render();
+        },
+        render: function(){
+          this.$el.html( this.template(this.model.toJSON()));
+          return this;
+        }
+      });
+
+
+
+      var headerModel = new App.Models.Header;
+      var headerView = new App.Views.Header({model: headerModel });
+      headerView.el;
+      $(document.body).html(headerView.el);
+
+
+
   });
 
-  App.Views.Header = Backbone.View.extend({
-    tagName: 'header',
-    template: _.template( $('#novos-classicos-template-header').html()),
-    initialize: function(){
-      console.log(this.template(this.model.toJSON()));
-      this.render();
-    },
-    render: function(){
-      this.$el.html( this.template(this.model.toJSON()));
-      return this;
-    }
-  });
 
-  var headerModel = new App.Models.Header;
-  var headerView = new App.Views.Header({model: headerModel });
-  headerView.el;
-  $(document.body).html(headerView.el);
 
 });
